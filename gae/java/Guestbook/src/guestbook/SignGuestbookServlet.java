47e633660521b98b4462a20884b9ebe23fccd3f5
wa <callmewa@gmail.com>
2013-07-02 16:13:33 -0700

    added datastore

--- gae/java/Guestbook/src/guestbook/SignGuestbookServlet.java
+++ gae/java/Guestbook/src/guestbook/SignGuestbookServlet.java
@@ -1,8 +1,16 @@
 package guestbook;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.logging.Logger;
+
 import javax.servlet.http.*;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
@@ -17,15 +25,18 @@ public class SignGuestbookServlet extends HttpServlet{
     UserService userService = UserServiceFactory.getUserService();
     User user = userService.getCurrentUser();
     
+    String guestbookName = req.getParameter("guestbookName");
+    Key guestbookKey = KeyFactory.createKey("Guestbook", guestbookName);
     String content = req.getParameter("content");
-    if (content == null){
-      content = "(No greeting)";
-    }
-    if (user != null){
-      log.info("Greeting posted by user " + user.getNickname() + ": " + content);
-    } else{
-      log.info("Greeting posted anonymously: " + content);
-    }
-    resp.sendRedirect("/guestbook.jsp");
+    Date date = new Date();
+    Entity greeting = new Entity("Greeting", guestbookKey);
+    greeting.setProperty("user", user);
+    greeting.setProperty("date", date);
+    greeting.setProperty("content", content);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(greeting);
+    
+    resp.sendRedirect("/guestbook.jsp?guestbookName=" + guestbookName);
   }
 }
