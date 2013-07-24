4d28e5df584cef4472dbe039bff448a513d23837
wa <callmewa@gmail.com>
2013-07-24 12:46:41 -0700

    changed service to use proto-datastore

--- gae/python/end-tac-toe/models.py
+++ gae/python/end-tac-toe/models.py
@@ -1,106 +1,62 @@
-#!/usr/bin/python
-
-# Copyright (C) 2010-2013 Google Inc.
+# Copyright 2012 Google Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import random
+import re
 
-"""Helper model class for TicTacToe API.
-
-Defines models for persisting and querying score data on a per user basis and
-provides a method for returning a 401 Unauthorized when no current user can be
-determined.
-"""
-
-
-from google.appengine.ext import endpoints
 from google.appengine.ext import ndb
+from protorpc import messages
 
-from tictactoe_api_messages import ScoreResponseMessage
-
-
-TIME_FORMAT_STRING = '%b %d, %Y %I:%M:%S %p'
-
-
-def get_endpoints_current_user(raise_unauthorized=True):
-    """Returns a current user and (optionally) causes an HTTP 401 if no user.
-
-    Args:
-        raise_unauthorized: Boolean; defaults to True. If True, this method
-            raises an exception which causes an HTTP 401 Unauthorized to be
-            returned with the request.
-
-    Returns:
-        The signed in user if there is one, else None if there is no signed in
-        user and raise_unauthorized is False.
-    """
-    current_user = endpoints.get_current_user()
-    if raise_unauthorized and current_user is None:
-        raise endpoints.UnauthorizedException('Invalid token.')
-    return current_user
+from endpoints_proto_datastore.ndb import EndpointsAliasProperty
+from endpoints_proto_datastore.ndb import EndpointsModel
+from endpoints_proto_datastore.ndb import EndpointsUserProperty
 
 
-class Score(ndb.Model):
-    """Model to store scores that have been inserted by users.
+class Board(EndpointsModel):
+  state = ndb.StringProperty(required=True)
 
-    Since the played property is auto_now_add=True, Scores will document when
-    they were inserted immediately after being stored.
-    """
-    outcome = ndb.StringProperty(required=True)
-    played = ndb.DateTimeProperty(auto_now_add=True)
-    player = ndb.UserProperty(required=True)
+  def MoveOpponent(self):
+    free_indices = [match.start() for match in re.finditer('-', self.state)]
+    random_index = random.choice(free_indices)
+    result = list(self.state)  # Need a mutable object
+    result[random_index] = 'O'
+    self.state = ''.join(result)
+    
+class Order(messages.Enum):
+  WHEN = 1
+  TEXT = 2
+        
+class Score(EndpointsModel):
+  _message_fields_schema = ('id', 'outcome', 'played', 'player')
 
-    @property
-    def timestamp(self):
-        """Property to format a datetime object to string."""
-        return self.played.strftime(TIME_FORMAT_STRING)
+  outcome = ndb.StringProperty(required=True)
+  played = ndb.DateTimeProperty(auto_now_add=True)
+  player = EndpointsUserProperty(required=True, raise_unauthorized=True)
 
-    def to_message(self):
-        """Turns the Score entity into a ProtoRPC object.
+  def OrderSet(self, value):
+    if not isinstance(value, Order):
+      raise TypeError('Expected an enum, received: %s.' % (value,))
 
-        This is necessary so the entity can be returned in an API request.
-
-        Returns:
-            An instance of ScoreResponseMessage with the ID set to the datastore
-            ID of the current entity, the outcome simply the entity's outcome
-            value and the played value equal to the string version of played
-            from the property 'timestamp'.
-        """
-        return ScoreResponseMessage(id=self.key.id(),
-                                    outcome=self.outcome,
-                                    played=self.timestamp)
-
-    @classmethod
-    def put_from_message(cls, message):
-        """Gets the current user and inserts a score.
-
-        Args:
-            message: A ScoreRequestMessage instance to be inserted.
-
-        Returns:
-            The Score entity that was inserted.
-        """
-        current_user = get_endpoints_current_user()
-        entity = cls(outcome=message.outcome, player=current_user)
-        entity.put()
-        return entity
-
-    @classmethod
-    def query_current_user(cls):
-        """Creates a query for the scores of the current user.
-
-        Returns:
-            An ndb.Query object bound to the current user. This can be used
-            to filter for other properties or order by them.
-        """
-        current_user = get_endpoints_current_user()
-        return cls.query(cls.player == current_user)
+    if value == Order.WHEN:
+      super(Score, self).OrderSet('-played')
+    elif value == Order.TEXT:
+      super(Score, self).OrderSet('outcome')
+    else:
+      raise TypeError('Unexpected value of Order: %s.' % (value,))
+  
+  order = EndpointsAliasProperty(lambda: super(Score, self).OrderGet(), setter=OrderSet, property_type=Order, default=Order.WHEN)
+  #@EndpointsAliasProperty(setter=OrderSet, property_type=Order,
+  #                        default=Order.WHEN)
+  #def order(self):
+  #  return super(Score, self).order
--- gae/python/end-tac-toe/tictactoe_api.py
+++ gae/python/end-tac-toe/tictactoe_api.py
@@ -1,125 +1,64 @@
-#!/usr/bin/python
-
-# Copyright (C) 2010-2013 Google Inc.
+# Copyright 2012 Google Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-"""TicTacToe API implemented using Google Cloud Endpoints."""
-
-
-import random
-import re
-
-from google.appengine.ext import endpoints
 from protorpc import remote
 
+from google.appengine.ext import endpoints
+
+from models import Board
 from models import Score
-from tictactoe_api_messages import BoardMessage
-from tictactoe_api_messages import ScoresListRequest
-from tictactoe_api_messages import ScoresListResponse
-from tictactoe_api_messages import ScoreRequestMessage
-from tictactoe_api_messages import ScoreResponseMessage
-
-
-CLIENT_ID = 'YOUR-CLIENT-ID'
 
 
 @endpoints.api(name='tictactoe', version='v1',
                description='Tic Tac Toe API',
-               allowed_client_ids=[CLIENT_ID, endpoints.API_EXPLORER_CLIENT_ID])
+               allowed_client_ids=['YOUR-CLIENT-ID',
+                                   endpoints.API_EXPLORER_CLIENT_ID])
 class TicTacToeApi(remote.Service):
-    """Class which defines tictactoe API v1."""
 
-    @staticmethod
-    def add_move_to_board(board_state):
-        """Adds a random 'O' to a tictactoe board.
+  @Board.method(path='board', http_method='POST',
+                name='board.getmove')
+  def BoardGetMove(self, board):
+    if not (len(board.state) == 9 and set(board.state) <= set('OX-')):
+      raise endpoints.BadRequestException('Invalid board.')
+    board.MoveOpponent()
+    return board
 
-        Args:
-            board_state: String; contains only '-', 'X', and 'O' characters.
+  @Score.method(request_fields=('id',),
+                path='scores/{id}', http_method='GET',
+                name='scores.get')
+  def ScoresGet(self, score):
+    if not score.from_datastore:
+      raise endpoints.NotFoundException('Score not found.')
 
-        Returns:
-            A new board with one of the '-' characters converted into an 'O';
-            this simulates an artificial intelligence making a move.
-        """
-        result = list(board_state)  # Need a mutable object
+    if score.player != endpoints.get_current_user():
+      raise endpoints.ForbiddenException(
+          'You do not have access to this score.')
 
-        free_indices = [match.start()
-                        for match in re.finditer('-', board_state)]
-        random_index = random.choice(free_indices)
-        result[random_index] = 'O'
+    return score
 
-        return ''.join(result)
+  @Score.method(request_fields=('outcome',),
+                path='scores', http_method='POST',
+                name='scores.insert')
+  def ScoresInsert(self, score):
+    score.put()  # score.player already set since EndpointsUserProperty
+    return score
 
-    @endpoints.method(BoardMessage, BoardMessage,
-                      path='board', http_method='POST',
-                      name='board.getmove')
-    def board_get_move(self, request):
-        """Exposes an API endpoint to simulate a computer move in tictactoe.
-
-        Args:
-            request: An instance of BoardMessage parsed from the API request.
-
-        Returns:
-            An instance of BoardMessage with a single 'O' added to the board
-            passed in.
-        """
-        board_state = request.state
-        if not (len(board_state) == 9 and set(board_state) <= set('OX-')):
-            raise endpoints.BadRequestException('Invalid board.')
-        return BoardMessage(state=self.add_move_to_board(board_state))
-
-    @endpoints.method(ScoresListRequest, ScoresListResponse,
-                      path='scores', http_method='GET',
-                      name='scores.list')
-    def scores_list(self, request):
-        """Exposes an API endpoint to query for scores for the current user.
-
-        Args:
-            request: An instance of ScoresListRequest parsed from the API
-                request.
-
-        Returns:
-            An instance of ScoresListResponse containing the scores for the
-            current user returned in the query. If the API request specifies an
-            order of WHEN (the default), the results are ordered by time from
-            most recent to least recent. If the API request specifies an order
-            of TEXT, the results are ordered by the string value of the scores.
-        """
-        query = Score.query_current_user()
-        if request.order == ScoresListRequest.Order.TEXT:
-            query = query.order(Score.outcome)
-        elif request.order == ScoresListRequest.Order.WHEN:
-            query = query.order(-Score.played)
-        items = [entity.to_message() for entity in query.fetch(request.limit)]
-        return ScoresListResponse(items=items)
-
-    @endpoints.method(ScoreRequestMessage, ScoreResponseMessage,
-                      path='scores', http_method='POST',
-                      name='scores.insert')
-    def scores_insert(self, request):
-        """Exposes an API endpoint to insert a score for the current user.
-
-        Args:
-            request: An instance of ScoreRequestMessage parsed from the API
-                request.
-
-        Returns:
-            An instance of ScoreResponseMessage containing the score inserted,
-            the time the score was inserted and the ID of the score.
-        """
-        entity = Score.put_from_message(request)
-        return entity.to_message()
-
+  @Score.query_method(query_fields=('limit', 'order', 'pageToken'),
+                      user_required=True,
+                      path='scores', name='scores.list')
+  def ScoresList(self, query):
+    return query.filter(Score.player == endpoints.get_current_user())
 
 APPLICATION = endpoints.api_server([TicTacToeApi],
                                    restricted=False)
