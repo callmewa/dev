7a9379917d90974775f9888f818f1f7a06cdec02
wa <callmewa@gmail.com>
2013-08-04 16:12:44 -0700

    add custom widget (TextViewBordered)

---
+++ andr/dynamic-listview-picasa/src/android/widget/ext/TextViewBordered.java
@@ -0,0 +1,43 @@
+package android.widget.ext;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import com.example.listview.R;
+
+/**
+ * Created by callmewa on 8/4/13.
+ */
+public class TextViewBordered extends TextView {
+
+    public TextViewBordered(Context context) {
+        super(context, null);
+        initWithColor(null);
+    }
+
+    public TextViewBordered(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        TypedArray a;
+        a = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.TextViewBordered, 0, 0);
+
+        String border_color = a.getString(R.styleable.TextViewBordered_border_color);
+        a.recycle();
+        initWithColor(border_color);
+    }
+
+    private void initWithColor(String color){
+        if(color == null){
+            setBackgroundResource(R.drawable.border);
+        }else{
+            GradientDrawable drawable = (GradientDrawable)getResources().getDrawable(R.drawable.border);
+            drawable.setStroke(1, Color.parseColor(color));
+            setBackground(drawable);
+        }
+    }
+}
---
+++ andr/dynamic-listview-picasa/src/com/example/listview/gravity_test.java
@@ -0,0 +1,15 @@
+package com.example.listview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by callmewa on 8/4/13.
+ */
+public class gravity_test extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.gravity_test);
+    }
+}
