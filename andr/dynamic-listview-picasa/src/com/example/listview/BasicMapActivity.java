feb24133d081e84042e2aa8bde1f0534c216e397
wa <callmewa@gmail.com>
2013-08-22 22:29:18 -0700

    more sandbox play

--- andr/dynamic-listview-picasa/src/com/example/listview/BasicMapActivity.java
+++ andr/dynamic-listview-picasa/src/com/example/listview/BasicMapActivity.java
@@ -16,13 +16,26 @@
 
 package com.example.listview;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.Arrays;
 
 /**
  * This shows how to create a simple activity with a map and a marker on the map.
@@ -35,18 +48,92 @@ public class BasicMapActivity extends FragmentActivity {
      * Note that this may be null if the Google Play services APK is not available.
      */
     private GoogleMap mMap;
+    private SupportMapFragment mMapFragment;
+    static boolean setup = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.basic_demo);
-        setUpMapIfNeeded();
+
+        if(savedInstanceState == null)setUpMapIfNeeded();
+
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        setUpMapIfNeeded();
+        //setUpMapIfNeeded();
+        mMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+        if(mMapFragment!=null){
+
+            mMap = mMapFragment.getMap();
+            Marker marker = mMap.addMarker(new MarkerOptions()
+                    //37° 46' 30" N / 122° 25' 5" W
+                    .position(new LatLng(37.7756, -122.4193))
+                    .title("Hello world")
+                    .snippet("Population: 4,137,400")
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+            mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener(){
+                @Override
+                public boolean onMarkerClick(Marker marker) {
+                    marker.setTitle("clicked");
+                    return false;
+                }
+            });
+            mMap.setMyLocationEnabled(true);
+
+            PolylineOptions rectOptions = new PolylineOptions()
+                    .add(new LatLng(37.35, -122.0))
+                    .add(new LatLng(37.45, -122.0))  // North of the previous point, but at the same longitude
+                    .add(new LatLng(37.45, -122.2))  // Same latitude, and 30km to the west
+                    .add(new LatLng(37.35, -122.2))  // Same longitude, and 16km to the south
+                    .add(new LatLng(37.35, -122.0))
+                    .color(Color.RED); // Closes the polyline.
+
+            // Get back the mutable Polyline
+            //Polyline polyline = mMap.addPolyline(rectOptions);
+
+            // Instantiates a new Polygon object and adds points to define a rectangle
+            PolygonOptions recOptions = new PolygonOptions()
+                    .add(new LatLng(37.35, -122.0),
+                            new LatLng(37.45, -122.0),
+                            new LatLng(37.45, -122.2),
+                            new LatLng(37.35, -122.2),
+                            new LatLng(37.35, -122.0))
+                    //.strokeColor(Color.RED)
+                    //.fillColor(Color.BLUE)
+                    ;
+
+// Get back the mutable Polygon
+            //Polygon polygon = mMap.addPolygon(recOptions);
+
+            Polygon polygon = mMap.addPolygon(new PolygonOptions()
+                    .add(new LatLng(0, 0), new LatLng(0, 5), new LatLng(3, 5))
+                    .strokeColor(Color.RED)
+                    .fillColor(Color.BLUE));
+
+            mMap.addPolygon(new PolygonOptions()
+                    .add(new LatLng(0, 0), new LatLng(0, 5), new LatLng(3, 5), new LatLng(3, 0), new LatLng(0, 0))
+                    .addHole(Arrays.asList(new LatLng(1, 1), new LatLng(1, 2), new LatLng(2, 2), new LatLng(2, 1), new LatLng(1, 1)))
+                    .fillColor(Color.BLUE));
+
+            // Instantiates a new CircleOptions object and defines the center and radius
+            CircleOptions circleOptions = new CircleOptions()
+                    .center(new LatLng(37.4, -122.1))
+                    .radius(1000); // In meters
+
+// Get back the mutable Circle
+            Circle circle = mMap.addCircle(circleOptions);
+
+
+            Polyline line = mMap.addPolyline(new PolylineOptions()
+                    .add(new LatLng(-37.81319, 144.96298), new LatLng(-31.95285, 115.85734))
+                    .width(25)
+                    .color(Color.BLUE)
+                    .geodesic(true));
+        }
     }
 
     /**
@@ -66,15 +153,24 @@ public class BasicMapActivity extends FragmentActivity {
      */
     private void setUpMapIfNeeded() {
         // Do a null check to confirm that we have not already instantiated the map.
-        if (mMap == null) {
+        //if (mMapFragment == null) {
+        //    setup = !setup;
             // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-                    .getMap();
-            // Check if we were successful in obtaining the map.
-            if (mMap != null) {
-                setUpMap();
-            }
-        }
+            //mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+
+            GoogleMapOptions options = new GoogleMapOptions();
+            options.mapType(GoogleMap.MAP_TYPE_NORMAL)
+                    .compassEnabled(true)
+                    .rotateGesturesEnabled(false)
+                    .tiltGesturesEnabled(false);
+
+            mMapFragment = SupportMapFragment.newInstance(options);
+            //mMapFragment.setRetainInstance(false);
+            View container = findViewById(R.id.fragment_container);
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, mMapFragment, "map").commit();
+
+            System.out.print("");
+        //}
     }
 
     /**
@@ -84,6 +180,10 @@ public class BasicMapActivity extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
+
         mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        //mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
     }
+
 }
